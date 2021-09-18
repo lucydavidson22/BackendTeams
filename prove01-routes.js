f6feff29d1ab8c37f4c1ba435d6535ca260f9278
lucydavidson22 <71901355+lucydavidson22@users.noreply.github.com>
2021-09-18 02:52:28 -0400

    name adds to second page

--- prove01-routes.js
+++ prove01-routes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+const users = ['Dummy User 1', 'Dummy User 2', 'Dummy User 3', 'Bilbo', 'Frodo', 'Sam'];
 const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
@@ -7,16 +8,30 @@ const requestHandler = (req, res) => {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Greetings Traveler!</title></head>');
-        res.write('<body><p>You must give me a name if you are to proceed.</p><form action="/create-user" method="post" ><input type="text" name="username"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
+        res.write('<body>');
+        // res.write('<h1>Greetings Weary Traveler!</h1>');
+        res.write('<h1>You Shall Not Pass!</h1>');
+        res.write('<h3>Unless you give me your name.</h3>');
+        res.write('<p>Then I guess it would be okay.</p>');
+        res.write('<form action="/create-user" method="post"><input type="text" name="username" placeholder="Travelers Name"><button type="submit">Send</button></form>');
         return res.end();
     }
     if (url === '/users'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Greetings Traveler!</title></head>');
-        res.write('<body><ul><li>Dummy User 1</li><li>Dummy User 2</li><li>Dummy User 3</li><li>Bilbo</li><li>Frodo</li><li>Sam</li></ul></body>');
-        res.write('');
+        
+        res.write('<body>');
+        res.write(`<h1 id="id1">Welcome Traveler ${users.slice(-1)}!!</h1>`);
+        res.write('<h2>A list of other travelers that have passed through these parts.</h2>');
+        res.write('<ul>');
+        for (const names of users) {
+            res.write(`<li>${names}</li>`);
+        }
+        res.write('</ul>');
+        res.write(`<button type="button", onclick="document.getElementById('id1').style.color = 'red'">Click Me!</button>`);
+        res.write('</body>');
+
         res.write('</html>');
         return res.end();
     }
@@ -27,12 +42,14 @@ const requestHandler = (req, res) => {
         });
         req.on('end', () =>{
             const parsedBody = Buffer.concat(body).toString();
+            const Username = parsedBody.split('=')[1];
+
             console.log(parsedBody.split('=')[1]);
+            users.push(Username);
         });
         res.statusCode = 302;
         res.setHeader('Location' , '/users'); //works for changing the landing location, which I don't know if
         res.end();                              // we were allowed to do that but I wanted to.
     }
 }
-
 module.exports = requestHandler;
--- prove01.js
+++ prove01.js
@@ -4,4 +4,5 @@ const routes = require('./prove01-routes');
 
 const server = http.createServer(routes);
 
-server.listen(3000);        //3000 sometimes shows a message that it is already in use. 
+server.listen(3000);        //3000 sometimes shows a message that it is already in use, 
+                            //but it seems to be doing okay so far with this project. 
