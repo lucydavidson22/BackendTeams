fc8aac556d974223188face30e71bf35d249b40a
lucydavidson22 <71901355+lucydavidson22@users.noreply.github.com>
2021-09-21 12:34:28 -0400

    teamActivity2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,41 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [];
+
+router.post('/addUser', (req, res, next) =>  {
+  users.push({name:req.body.username});
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  let name = {name: req.body.removeUsername};
+  var nameIndex = -1;
+  for (let index = 0; index < users.length; index++) {
+    if (users[index].name === name.name){
+      nameIndex = index;
+      break;
+    }
+  }
+  if (nameIndex > -1) {
+    users.splice(nameIndex, 1);
+  }
+  console.log(users);
+  res.redirect('/ta02');
+})
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users
   });
 });
 
-module.exports = router;
+
+
+// module.exports = router;
+exports.routes = router;
+exports.users = users;
